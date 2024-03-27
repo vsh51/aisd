9189f1b6a4928c08d4a2635d5d0caebbcc59af57
vsh51 <volodymyr.shpyrka@lnu.edu.ua>
2024-03-27 09:20:05 +0200

    started developing queue

--- queue/Queue.cpp
+++ queue/Queue.cpp
@@ -13,13 +13,54 @@ private:
 
         node(int priority = 0, datatype data = datatype(), unsigned int id = 0, node* prev = nullptr, node* next = nullptr):
             priority(priority), data(data), id(id), prev(prev), next(next) {};
-    }
+    };
 
     node* head;
+    node* tail;
     unsigned int amount_of_items;
 
+public:
+    Queue() {
+        this->head = nullptr;
+        this->tail = nullptr;
+    };
+
+    bool isEmpty() {
+        return head == nullptr;
+    };
+
+    void add(int id, datatype data, int priority = 0) {
+        if (isEmpty()) {
+            head = new node(priority, data, id, nullptr, nullptr);
+            tail = head;
+        }
+        else {
+            node* tmpptr = head;
+            while (tmpptr->next != nullptr && tmpptr->next->priority >= priority) {
+                tmpptr = tmpptr->next;
+            }
+            node* insert = new node(priority, data, id, tmpptr, tmpptr->next);
+            if (tmpptr->next != nullptr) {
+                tmpptr->next->prev = insert;
+                tmpptr->next = insert;
+            }
+            else {
+                tmpptr->next = insert;
+                tail = insert;
+            }
+        }
+    };
+
+    datatype get() {
+        datatype rt = tail->data;
+
+        return rt;
+    };
 };
 
 int main () {
-
+    Queue<int> q;
+    q.add(3, 2, 3);
+    q.add(2, 47, 4);
+    cout << q.get();
 };
