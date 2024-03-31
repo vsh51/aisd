d4076b7c4af87fc461b1a4e10c0c971724a21d57
vsh51 <volodymyr.shpyrka@lnu.edu.ua>
2024-03-31 20:49:55 +0300

    implemented 'contains' method

--- markov/Shpyrka_markov.cpp
+++ markov/Shpyrka_markov.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	cout << "AA" << "\n";
-}
+class MarkovString {
+private:
+	string init_str;
+	string resulting_str;
+	string termination;
+	string* rules;
+
+public:
+	MarkovString(string init_str) : init_str(init_str) {};
+	~MarkovString() {
+		delete[] rules;
+	};
+
+private:
+	bool contains(string substr) {		
+		int substr_counter = 0;
+		for (int i = 0; i < resulting_str.length(); ++i) {
+			if (resulting_str[i] == substr[substr_counter]) {
+				++substr_counter;
+				if (substr.length() == substr_counter) return true;
+			}
+			else {
+				substr_counter = 0;
+			};
+		};
+		return false;
+	};
+};
